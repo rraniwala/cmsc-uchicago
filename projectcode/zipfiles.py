792727c10586f09371b14aa7f89282d431d805e2
rraniwala <63739610+rraniwala@users.noreply.github.com>
2022-06-13 11:27:47 -0400

    Delete projectcode directory

--- projectcode/zipfiles.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-import zipfile
-
-zip = zipfile.ZipFile('new_outputs.zip', 'w')
-for f in os.listdir(sys.argv[1]):
-    zip.write(os.path.join(sys.argv[1], f), f)
-zip.close()
